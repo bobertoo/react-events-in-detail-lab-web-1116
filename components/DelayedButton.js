86f36791d8e0c5e0670c48fc703623591ca83b59
Vinny Martinez <vinny@vinnymartinez.com>
2017-05-13 17:50:09 -0400

    forgotten commit

--- components/DelayedButton.js
+++ components/DelayedButton.js
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class DelayedButton extends React.Component {
+  constructor(){
+    super()
+
+    this.clickShit = this.clickShit.bind(this)
+  }
+  clickShit(event){
+    event.persist()
+    setTimeout(() => {this.props.onDelayedClick(event)}, this.props.delay)
+  }
   render(){
     return (
       <div>
-
+        <button onClick={this.clickShit}></button>
       </div>
     )
   }
